efd3b31dfee6f963f76fc9dd10a561fe7fd524e4
FaisalNadaf <faisalfnadaf@gmail.com>
2024-12-14 22:41:37 +0530

    3rd-Sem-FSD-Workshop

---
+++ day3/code1/script.js
@@ -0,0 +1,19 @@
+/** @format */
+// function changeText() {
+// 	document.getElementById("demo").innerText = "hello !";
+// }
+
+let name = "demo";
+let number = 563538438;
+let isTrue = false;
+let notAssigned;
+let emptyValue = null;
+console.log(typeof name, typeof number);
+
+let a = 10;
+let b = 5;
+console.log(a + b);
+console.log(a - b);
+console.log(a * b);
+console.log(a / b);
+console.log(a % b);
